1432303cd10e29171f51d4afe4597d7e19f70cb6
imranhossainppip <imranhossainppip@gmail.com>
2021-11-13 18:57:40 +0600

    some changed

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
-var test = new Vue({
-		el: "#test",
-		data: {
-		  name: "Imran",
-		  userAge: 20,
-		  allowedAge: 25
-		}
+
+
+// const getVar = () => {
+// 	setTimeout(function(){
+// 		console.log('A Function Take some time');
+// 	}, 3000)
+// }
+// const printSomething = () => {
+// 	console.log('Another Function');
+// }
+// getVar();
+// printSomething();
+// const getVarC = (callback) => {
+// 	setTimeout(function() {
+// 	   console.log('A Function that takes some time');
+// 	   callback();
+// 	}, 3000)
+//  }
+ 
+//  const printSomethingC = () => {
+// 	console.log('Another Function');
+//  }
+ 
+//  getVarC(printSomethingC);
+const aPromise = control => { 
+	return new Promise((resolve, reject) => {
+	   setTimeout(() => {
+		  if(control) {
+			 resolve();
+		  } else {
+			 reject();
+		  }
+	   }, 3000)
 	})
+ }
+ aPromise(false)
+  .then(() => {
+     console.log('This is a Success');
+  })
+  .catch(() => {
+     console.log('This is a Failure');
+  })
